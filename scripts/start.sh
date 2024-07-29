da5432f83db4704176afa56bc39ca904f615e1bc
Luiz Ricardo <luizrgfelipe@gmail.com>
2024-07-28 22:40:20 -0300

    🧹 CHORE: add docker file

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+chmod +x /usr/local/bin/wait-for-it.sh
+
+/usr/local/bin/wait-for-it.sh mysql:3306
+/usr/local/bin/wait-for-it.sh rabbitmq:5672
+/usr/local/bin/wait-for-it.sh redis:6379
+
+npx prisma migrate dev
+npm start
